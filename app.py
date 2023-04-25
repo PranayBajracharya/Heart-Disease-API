9d060b47fd2807a2ca23387ae66f44c5739a4ed0
pranay.bajracharya <law.pran99@gmail.com>
2023-04-25 23:48:54 +0545

    normalized in backend

--- app.py
+++ app.py
@@ -2,13 +2,31 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
 import pickle
+import csv
 
 app = Flask(__name__)
 
 CORS(app)
 
+MAX_TRESTBPS = 200
+MIN_TRESTBPS = 94
+
+MAX_CHOL = 564
+MIN_CHOL = 126
+
+MAX_THALACH = 202
+MIN_THALACH = 71
+
 svm_modal = pickle.load(open('SVM.pkl', 'rb'))
 decision_tree_modal = pickle.load(open('DecisionTree.pkl', 'rb'))
+
+def normalize(value, max, min):
+  normalized_value = (value - min) / (max - min)
+  if normalized_value > 1:
+    normalized_value = 1
+  if normalized_value < 0:
+    normalized_value = 0
+  return normalized_value
 
 def sigmoid(x):
   return 1/(1 + np.exp(-x))
@@ -31,8 +49,9 @@ def decisionTree(input):
 
 
 def logisticRegression(input):
-  B = -0.3089914854382788
-  W = np.array([[ 0.09129111],[-6.33505535],[ 8.71898628],[-0.12598532],[-0.01500191],[ 0.24280076],[ 1.81233911],[ 0.17489223],[-5.41853826],[-6.05298511],[ 1.46312178],[-9.01810412],[-7.43117029]])
+  B = 1.4529196202976526
+  # W = np.array([[-0.01184615],[-1.63559982],[ 1.00394245],[-0.76612724],[-0.32547678],[-0.32404015],[ 0.63957675],[ 2.16091011],[-1.06465917],[-1.76134254],[ 0.66852324],[-0.86071214]])
+  W = np.array([[-0.03054175],[-1.75988952],[ 0.87459045],[-0.88589462],[-0.65953262],[-0.19379372],[ 2.7781281 ],[-1.18520633]])
 
   Z = np.dot(W.T, input) + B
 
@@ -44,6 +63,61 @@ def logisticRegression(input):
     prediction = 0
   return prediction  
 
+# def accuracy(age, sex, cp, trestbps, chol, fbs, thalach, exang):
+#   age =int(age)
+#   sex =int(sex)
+#   cp =int(cp)
+#   trestbps =float(trestbps)
+#   chol =float(chol)
+#   fbs =int(fbs)
+#   thalach =float(thalach)
+#   exang = int(exang)
+#   norm_bps = normalize(trestbps, MAX_TRESTBPS, MIN_TRESTBPS)
+#   norm_chol = normalize(chol, MAX_CHOL, MIN_CHOL)
+#   norm_thalach = normalize(thalach, MAX_THALACH, MIN_THALACH)
+
+#   # input_data = (60,1,0,145,174,0,1,125,1,2.6,0,0,3)
+#   input_data = (age,sex,cp,norm_bps,norm_chol,fbs,norm_thalach,exang)
+
+
+#   logistic_regression = logisticRegression(input_data)
+#   svm = SVM(input_data)
+#   dt = decisionTree(input_data)
+#   result = (logistic_regression + svm + dt) > 1
+#   if result:
+#     result = 1
+#   else:
+#     result = 0
+#   return result
+
+# filename = "hhd_after.csv"
+
+# def asd():
+#   with open(filename, 'r') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     next(csvreader)
+#     TP = 0
+#     TN = 0
+#     FP = 0
+#     FN = 0
+#     for row in csvreader:
+#       # print(row[:-1])
+#       age, sex, cp, trestbps, chol, fbs, thalach, exang = row[:-1]
+      
+#       expected_result = float(row[-1])
+#       actual_result = accuracy(age, sex, cp, trestbps, chol, fbs, thalach, exang)
+
+#       if expected_result == 1 and actual_result == 1:
+#         TP += 1
+#       elif expected_result ==0 and actual_result == 0:
+#         TN += 1
+#       elif expected_result == 1 and actual_result == 0:
+#         FN += 1
+#       elif expected_result == 0 and actual_result == 1:
+#         FP +=1
+
+#     print(TP, TN, FN, FP)
+
 
 @app.route('/', methods=['POST'])
 def result():
@@ -51,27 +125,33 @@ def result():
 
   age = data['age']
   sex = data['sex']
-  ca = data['ca']
-  chol = data['chol']
   cp = data['cp']
-  exang = data['exang']
-  fbs = data['fbs']
-  oldpeak = data['oldpeak']
-  restecg = data['restecg']
-  slope = data['slope']
-  thalach = data['thalach']
-  thal = data['thal']
   trestbps = data['trestbps']
+  chol = data['chol']
+  fbs = data['fbs']
+  thalach = data['thalach']
+  exang = data['exang']
+  # oldpeak = data['oldpeak']
+  # restecg = data['restecg']
+  # slope = data['slope']
+  # thal = data['thal']
+  # ca = data['ca']
 
-  # input_data = (70,1,0,145,174,0,1,125,1,2.6,0,0,3)
-  input_data = (age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
+  norm_bps = normalize(trestbps, MAX_TRESTBPS, MIN_TRESTBPS)
+  norm_chol = normalize(chol, MAX_CHOL, MIN_CHOL)
+  norm_thalach = normalize(thalach, MAX_THALACH, MIN_THALACH)
+
+  # input_data = (60,1,0,145,174,0,1,125,1,2.6,0,0,3)
+  input_data = (age,sex,cp,norm_bps,norm_chol,fbs,norm_thalach,exang)
 
 
   logistic_regression = logisticRegression(input_data)
   svm = SVM(input_data)
   dt = decisionTree(input_data)
+  final_result = (logistic_regression + svm + dt) > 1
 
-  return jsonify({'logistic_regression': str(logistic_regression), 'svm': str(svm), 'dt': str(dt)})
+  return jsonify({'logistic_regression': str(logistic_regression), 'svm': str(svm), 'dt': str(dt), 'result': str(final_result)})
 
 if __name__ == '__main__':
+  # asd()
   app.run(port=8000, debug=True)
